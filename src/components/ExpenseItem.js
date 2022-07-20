aa1a7fec7662fdd2717320c9c55ed78546e8aa55
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-07-20 18:37:53 -0500

    added css file for styling expenseitem js file and altered styling using classes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+    return (<div  className="expense-item">
+        <div>Date: July 20th</div>
+        <div className="expense-item__description">
+            <h2>Title: Car Payment</h2>
+            <div className="expense-item__price">Amount:$175.53</div>
+        </div>
+            </div>);
 }
 
 export default ExpenseItem;
