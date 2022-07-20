edebf28dd792a46b2582209379cc3321f7b9cb42
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-07-19 21:14:16 -0500

    began React lesson, working with components and JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from "./components/ExpenseItem";
+//This function is what is being imported into index.js and then displayed in the div chosen by id="root" from index.html
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Hello, World!</p>
+        <ExpenseItem></ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
