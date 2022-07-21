b6e4ee7d30dfae3373ff40b9922d17289e12cf5f
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-07-21 17:59:49 -0500

    added a new component to decrease code needed in ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,22 @@
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
-    const month = props.date.toLocaleString('en-US',{month: 'long'});
-    const day = props.date.toLocaleString('en-US',{day: '2-digit'});
-    const year = props.date.getFullYear();
 
+    return (
+        <div  className="expense-item">
 
+        {/*".date" comes from App.js, App -> ExpenseDate -> ExpenseItem*/}
+        <ExpenseDate date={props.date}></ExpenseDate>
 
-    return (<div  className="expense-item">
-        <div>{month}</div>
-        <div>{day},</div>
-        <div>{year}</div>
         <div className="expense-item__description">
             {/*"props" taken from function parameter
             "title" taken from App.js attribute in ExpenseItem element*/}
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
-            </div>);
+
+        </div>);
 }
 
 export default ExpenseItem;
