9ccdb494ff955c04b1a39b795222e8668d32d365
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-07-20 19:25:32 -0500

    covered props and how to properly reference them for use

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import ExpenseItem from "./components/ExpenseItem";
 //This function is what is being imported into index.js and then displayed in the div chosen by id="root" from index.html
 function App() {
+  const expenses = [
+    {id:1,title:'paper',dollarAmount:5,date: new Date(2020,1,5)},
+    {id:2,title:'cups',dollarAmount:10,date: new Date(2020,5,13)},
+    {id:3,title:'towels',dollarAmount:20,date: new Date(2020,8,22)},
+    {id:4,title:'soap',dollarAmount:18,date: new Date(2020,11,19)}
+  ];
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +19,18 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> Hello, World!</p>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem title={expenses[0].title}
+                     amount={expenses[0].dollarAmount}
+                     date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[1].title}
+                     amount={expenses[1].dollarAmount}
+                     date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title}
+                     amount={expenses[2].dollarAmount}
+                     date={expenses[2].date}></ExpenseItem>
+        <ExpenseItem title={expenses[3].title}
+                     amount={expenses[3].dollarAmount}
+                     date={expenses[3].date}></ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
+function ExpenseItem(props){
+    const expenseDate = new Date(2022,7,15);
+    const expenseTitle = 'Car Payment';
+    const expenseAmount = 175.53;
+
     return (<div  className="expense-item">
-        <div>Date: July 20th</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>Title: Car Payment</h2>
-            <div className="expense-item__price">Amount:$175.53</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
             </div>);
 }
