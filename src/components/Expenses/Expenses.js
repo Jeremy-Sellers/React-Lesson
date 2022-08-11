556e27af451925db70b7683c2f762e3012704041
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-08-11 14:49:45 -0500

    refactored code to display expenses based on array instead of hardcoding

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -26,18 +26,10 @@ function Expenses(props){
 
                 {/*References ExpenseItem component */}
             {/* "props" from parameter ".items" from App.js attribute [x].value is grabbed from the items values passed in from App.js */}
-            <ExpenseItem title={props.items[0].title}
-                         amount={props.items[0].dollarAmount}
-                         date={props.items[0].date}></ExpenseItem>
-            <ExpenseItem title={props.items[1].title}
-                         amount={props.items[1].dollarAmount}
-                         date={props.items[1].date}></ExpenseItem>
-            <ExpenseItem title={props.items[2].title}
-                         amount={props.items[2].dollarAmount}
-                         date={props.items[2].date}></ExpenseItem>
-            <ExpenseItem title={props.items[3].title}
-                         amount={props.items[3].dollarAmount}
-                         date={props.items[3].date}></ExpenseItem>
+                {props.items.map(expense => <ExpenseItem title={expense.title}
+                                                         amount={expense.dollarAmount}
+                                                         date={expense.date}
+                                                                            />)}
         </Card>
         </div>
     )
