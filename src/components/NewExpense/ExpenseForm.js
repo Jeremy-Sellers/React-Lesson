1bb10b00c982172a91b31ce4d2844bd2549e36c2
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-08-12 17:12:07 -0500

    fixed date input to show correct date instead of one day prior to chosne date

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -25,7 +25,7 @@ function ExpenseForm(props){
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate + 'T00:00:00') //adding string fixes date from being entered as one day prior to chosen date
         }
 
         console.log(expenseData);
